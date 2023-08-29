d6dd0df023ba5e7faaa226c2700f51e5d32beac6
mohammad2745 <mohammadfcc2745@gmail.com>
2023-08-29 18:42:35 +0600

    Lesson-2 video 9

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -4,54 +4,50 @@ const { StringDecoder } = require('string_decoder');
 const routes = require('../routes');
 const { notFoundHandler } = require('../handlers/routeHandlers/notFoundHandler');
 
-// module scaffolding
+// modue scaffolding
 const handler = {};
 
 handler.handleReqRes = (req, res) => {
-    // request handle
-    // get url and parse it
-    const parseUrl = url.parse(req.url, true);
-    const path = parseUrl.pathname;
-    const trimPath = path.replace(/^\/+|\/+$/g, '');
+    // request handling
+    // get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
     const method = req.method.toLowerCase();
-    const queryStringObject = parseUrl.query;
+    const queryStringObject = parsedUrl.query;
     const headersObject = req.headers;
 
     const requestProperties = {
-        parseUrl,
+        parsedUrl,
         path,
-        trimPath,
+        trimmedPath,
         method,
         queryStringObject,
         headersObject,
-    }
+    };
 
     const decoder = new StringDecoder('utf-8');
     let realData = '';
 
-    const chosenHandler = routes[trimPath] ? routes[trimPath] : notFoundHandler;
-
-    chosenHandler(requestProperties, (statusCode, payload) => {
-        statusCode = typeof(statusCode) === 'number' ? statusCode : 500;
-        payload = typeof(payload) === 'object' ? payload : {};
-
-        const payloadString = JSON.stringify(payload);
-
-        // return final response 
-        res.writeHead(statusCode);
-        res.end(payloadString);
-    });
+    const chosenHandler = routes[trimmedPath] ? routes[trimmedPath] : notFoundHandler;
 
     req.on('data', (buffer) => {
-        realData = decoder.write(buffer);
+        realData += decoder.write(buffer);
     });
 
     req.on('end', () => {
         realData += decoder.end();
-        console.log(realData);
 
-        // response handle
-        res.end('hello world');
+        chosenHandler(requestProperties, (statusCode, payload) => {
+            statusCode = typeof statusCode === 'number' ? statusCode : 500;
+            payload = typeof payload === 'object' ? payload : {};
+
+            const payloadString = JSON.stringify(payload);
+
+            // return the final response
+            res.writeHead(statusCode);
+            res.end(payloadString);
+        });
     });
 };
 
